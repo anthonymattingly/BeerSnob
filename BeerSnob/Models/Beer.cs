4f9a5356b4259b5a17bdca0c0dd1e80ebbb3d8fd
anthonymattingly <amattingly100@gmail.com>
2018-03-07 17:58:43 -0500

    Initial Commit

---
+++ BeerSnob/Models/Beer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeerSnob.Models
+{
+    public class Beer
+    {
+        public int BeerId { get; set; }
+
+        public string BeerName { get; set; }
+
+        public string Country { get; set; }
+
+        public double PercentABV { get; set; }
+
+        public double Rating { get; set; }
+
+        public string Description { get; set; }
+    }
+}
